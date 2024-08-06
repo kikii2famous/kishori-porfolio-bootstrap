e97d93b10be72e7c1cf7de34630a438e9960855d
kikii2famous <kikiidadonn@gmail.com>
2024-08-06 16:04:46 +0000

    I built an age verification app for my home page.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+//console. log( 'Hello World!')
+//var prompt = prompt 'What is your name?')
+//console.log(prompt)
+
+
+
+function verifyAge(age){
+  var age = prompt('What is your age?');
+  console.log(age)
+
+  if(age <18) {
+    alert('you are a minor')
+  } else if (age >= 18 && age <= 60) {
+    alert('you are an adult')
+  } else {
+    alert('you are a senior citizen!')
+  }
+}
